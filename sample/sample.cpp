355fd2de6178b611d2c0b8a1cbbb8e403c83475d
Marei Kikukawa <contact@calmery.me>
2017-03-07 18:22:55 +0900

    Add sample

---
+++ sample/sample.cpp
@@ -0,0 +1,12 @@
+extern "C" {
+    int fib( int n ){
+        switch( n ){
+            case 0: 
+            	return 0;
+            case 1: 
+            	return 1;
+            default: 
+            	return fib( n - 2 ) + fib( n - 1 );
+        }
+    }
+}
